55026e6ee56941e19b7899dd86fb1a8c756f11dd
Joose Helle <joose.helle@gmail.com>
2022-11-27 13:17:13 +0200

    process polygons in parallel

--- src/smooth_simplify_polygons.rs
+++ src/smooth_simplify_polygons.rs
@@ -1,8 +1,9 @@
 use geo::{ChaikinSmoothing, Polygon, Simplify};
+use rayon::prelude::*;
 
 pub fn smooth_simplify_polygons(polygons: Vec<Polygon>, epsilon_m: &f64) -> Vec<Polygon> {
     polygons
-        .into_iter()
+        .par_iter()
         .map(|p| p.chaikin_smoothing(2).simplify(epsilon_m))
         .collect()
 }
