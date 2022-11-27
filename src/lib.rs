904e554e4e291eff95a32970c29b8472e4fe8f4b
Joose Helle <joose.helle@gmail.com>
2022-11-27 14:45:51 +0200

    add smooth_and_simplify_polygons_from_wkts

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,35 @@
 use geojson::FeatureCollection;
 use pyo3::prelude::*;
+use wkt::ToWkt;
 
 mod make_fc_from_polygons;
 mod read_polygons_from_fc;
+mod read_polygons_from_wkt;
 mod smooth_simplify_polygons;
+
+pub fn smooth_and_simplify_polygons_from_wkts_impl(
+    wkts: &[&str],
+    simplify_tolerance_m: &f64,
+) -> Vec<String> {
+    smooth_simplify_polygons::smooth_simplify_polygons(
+        read_polygons_from_wkt::read_polygons_from_wkts(wkts),
+        simplify_tolerance_m,
+    )
+    .iter()
+    .map(|p| p.wkt_string())
+    .collect()
+}
+
+#[pyfunction]
+fn smooth_and_simplify_polygons_from_wkts(
+    polygon_wkts: Vec<&str>,
+    simplify_tolerance_m: f64,
+) -> PyResult<Vec<String>> {
+    Ok(smooth_and_simplify_polygons_from_wkts_impl(
+        &polygon_wkts,
+        &simplify_tolerance_m,
+    ))
+}
 
 pub fn smooth_and_simplify_polygon_fc_impl(
     geojson: &str,
@@ -26,6 +52,7 @@ fn smooth_and_simplify_polygon_fc(geojson: &str, simplify_tolerance_m: f64) -> P
 #[pymodule]
 fn mini_groove(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(smooth_and_simplify_polygon_fc, m)?)?;
+    m.add_function(wrap_pyfunction!(smooth_and_simplify_polygons_from_wkts, m)?)?;
     Ok(())
 }
 
