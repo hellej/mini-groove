4ff6f72228b27a67a3e877367ecf4f1fb5c3d1d6
Joose Helle <joose.helle@gmail.com>
2022-11-27 14:49:10 +0200

    update pyi

--- mini_groove.pyi
+++ mini_groove.pyi
@@ -1,3 +1,5 @@
+from typing import List
+
 
 def smooth_and_simplify_polygon_fc(geojson: str, simplify_tolerance_m: float) -> str:
     """
@@ -6,5 +8,17 @@ def smooth_and_simplify_polygon_fc(geojson: str, simplify_tolerance_m: float) ->
     Expects the coordinates of the features to be in meters.
     
     Returns GeoJSON FeatureCollection as string.
+    """
+    pass
+
+
+def smooth_and_simplify_polygons_from_wkts(
+    polygon_wkts: List[str],
+    simplify_tolerance_m: float
+) -> List[str]:
+    """Reads polygons from list of WKT Polygons, smoothens and simplifies them.
+    Supports only WKT Polygons. Expects the coordinates of the geometries to be in meters.
+
+    Returns simplified geometries as list of WKTs.
     """
     pass
